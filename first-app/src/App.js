5433349b425f5080e607b4d18f0fbdbbc4cc18d6
Rudy Schmitz <rschmitz@pnmg.com>
2018-02-22 15:09:21 -0600

    Add Sass functionality

--- first-app/src/App.js
+++ first-app/src/App.js
@@ -1,18 +1,19 @@
 import React, { Component } from 'react';
+import { render } from 'react-dom';
+import Greeting from './components/Greeting';
 import logo from './logo.svg';
-import './App.css';
+import './scss/theme.css';
 
 class App extends Component {
   render() {
     return (
-      <div className="App">
-        <header className="App-header">
-          <img src={logo} className="App-logo" alt="logo" />
-          <h1 className="App-title">Welcome to React</h1>
+      <div className="app">
+        <header className="app-header">
+          <img src={logo} className="app-logo" alt="logo" />
+
+          <h1 className="app-title">Welcome to React</h1>
         </header>
-        <p className="App-intro">
-          To get started, edit <code>src/App.js</code> and save to reload.
-        </p>
+        <Greeting />
       </div>
     );
   }
