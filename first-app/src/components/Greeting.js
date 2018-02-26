6e98ed8fbc9e3ab6228038dc76203959fc3ea077
Rudy Schmitz <rschmitz@pnmg.com>
2018-02-26 13:05:07 -0600

    Delete Sass, move html to public folder and update app.js

--- first-app/src/components/Greeting.js
+++ first-app/src/components/Greeting.js
@@ -1,9 +1,8 @@
 import React from 'react';
-import { render } from 'react-dom';
 
 class Greeting extends React.Component {
   render() {
-    return <h1>Rudy is cool</h1>;
+    return <p>Test Message</p>;
   }
 }
 
